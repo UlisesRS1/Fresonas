f4fe7ea117f7507bd0b7368d3dc6fa07dc4e533f
dianalvv <dianalopezvelardee@gmail.com>
2024-05-29 13:50:19 -0600

    primer commit

---
+++ header.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="style/estilo.css">
+</head>
+<body>
+<container-fluid>
+      <header class="encabezado text-center">
+        
+        <!-- <div class="text-center"> -->
+          <nav class="navbar navbar-expand-lg margen-nav fixed-top encabezado">
+            <div class="container-fluid justify-content-center">
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+                    <div class="navbar-nav justify-content-center mx-auto">
+                        <!-- <h4 class="d-block d-lg-none">Fresas con crema</h4> -->
+                        <div class="text-center mb-3 mb-lg-0">
+                            <img src="assets\images\logo.png" alt="logotipo" width="95" height="95">
+                        </div>
+                        <h3 class="mt-2 text-center fs-5 d-none d-lg-block margen-derecho" style="white-space: nowrap; overflow: hidden;">
+                            <span style="display: inline-block; max-width: 100%;">
+                                Fresas <br/> con <br/> crema
+                            </span>
+                        </h3>
+                        <a class="nav-link active text-center fs-5 my-0 my-lg-3" aria-current="page" href="#carousel">Inicio</a>
+                        <a class="nav-link text-center fs-5 my-0 my-lg-3" href="#cards">Menú</a>
+                        <a class="nav-link text-center fs-5 my-0 my-lg-3" href="#section">Recetas</a>
+                        <a class="nav-link text-center fs-5 my-0 my-lg-3" href="#galeria">Presentaciones</a>
+                        <a class="nav-link text-center fs-5 my-0 my-lg-3" href="#accordion">Preguntas frecuentes</a>
+                        <a class="nav-link text-center fs-5 my-0 my-lg-3" href="#formulario">Suscripción</a>
+                        <!-- Elemento de búsqueda -->
+                    </div>
+                </div>
+            </div>
+        </nav>
+        
+        <!-- </div> -->
+    </header>
+    
+        
+    </container-fluid>
+    
+
+
+
+    <script src="js/bootstrap.bundle.min.js"></script>
+</body>
+</html>
